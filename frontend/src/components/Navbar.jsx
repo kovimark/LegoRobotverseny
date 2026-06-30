bb9630f12269ae0e10e0745448c53462743cdd0f
Kovács Márk <kovacsm2@kkszki.hu>
2026-06-30 19:26:10 +0200

    bugfix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar({ user, userRole, authLoading, authError, onGoogleSignIn, onSignOut }) {
